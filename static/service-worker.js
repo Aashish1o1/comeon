aacee67f2a3d481a12e85dfdb453c1c5e9efc716
Aashish1o1 <sharmaaashish0004@gmail.com>
2025-11-24 18:29:07 +0530

    Add files via upload

---
+++ static/service-worker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'attendance-v1';
+const urlsToCache = [
+  '/scan',
+  '/static/style.css'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
